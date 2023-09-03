c5877b754be90cae7e633ddc5a8fb0f29720cb6e
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2023-09-03 16:17:42 -0700

    Variables and output

---
+++ basicStuff/src/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+
+	public static void main(String[] args) {
+		// Variable and outputting to console practice
+		
+		int number = 69;
+		System.out.println(number);
+		// for numbers there are also the types: short, long, float, byte
+		
+		double decimal = 3.14159;
+		System.out.println(decimal);
+		
+		char d = 'd';
+		System.out.println(d);
+		
+		boolean bool = false;
+		System.out.println(bool);
+		bool = true;
+		System.out.println(bool);
+	}
+
+}
