0473e39e6651dec9c4ea419cb9478acf1b9dbb81
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2023-09-03 16:28:08 -0700

    strings

---
+++ basicStuff/src/strings.java
@@ -0,0 +1,14 @@
+public class strings {
+	public static void main(String[] args) {
+		int integer = 3;
+		String text = "Hello";
+		String space = " ";
+		String name = "Bob";
+		
+		String greeting = text + space + name;
+		
+		System.out.println(greeting);
+		System.out.println("A" + ", " + "B");
+		System.out.println("Here is an integer: " + integer);
+	}
+}
