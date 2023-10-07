c620d6c3fe02b4ec96e7faeb33847df454c71873
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2023-10-06 17:10:42 -0700

    Loops

---
+++ Loops/src/Loops.java
@@ -0,0 +1,18 @@
+public class Loops {
+
+	public static void main(String[] args) {
+		// While loops
+		int number = 0;
+		while(number < 5) {
+			System.out.println("While loop: " + number);
+			number++;
+		}
+		// For loop
+		int number2 = 0;
+		for(int i=0; i<5; i++) {
+			System.out.println("For loop: " + number2);
+			number2++;
+		}
+	}
+
+}
