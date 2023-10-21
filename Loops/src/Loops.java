aa1a00e8d9b17461a1026c796a038c0eb4fbc88a
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2023-10-20 20:09:15 -0700

    Printf

--- Loops/src/Loops.java
+++ Loops/src/Loops.java
@@ -10,7 +10,8 @@ public class Loops {
 		// For loop
 		int number2 = 0;
 		for(int i=0; i<5; i++) {
-			System.out.println("For loop: " + number2);
+			// System.out.println("For loop: " + number2);
+			System.out.printf("For loop: %d\n", number2);
 			number2++;
 		}
 	}
